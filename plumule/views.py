cd2395c945fabb7a97e69f22303f953d7c8cf560
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-08-13 15:07:00 +0300

    feat: add api-cruds for all endpoints

--- plumule/views.py
+++ plumule/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Products, User, Orders
+from .serializers import ProductSerializer, UserSerializer, OrderSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializer
