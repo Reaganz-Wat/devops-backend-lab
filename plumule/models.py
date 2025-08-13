5ec4c7df0d086cdb73654b04ba2733b9a3a2332c
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-08-13 14:27:46 +0300

    feat: add projects models

--- plumule/models.py
+++ plumule/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.name} -> {self.price}"
+
+class Orders(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} -> {self.product.name}"
